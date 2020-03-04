d313c202d763b2c5850d1978e3f919e8168115a1
Riddhi Thummar <thummarriddhi8@gmail.com>
2020-03-04 12:26:26 -0500

    Add show action and gravatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+	def gravatar_for(user, options = { size: 80 })
+		# generate an MD5 hash
+		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+		size = options[:size]
+		# put its value to get the gravatar_url
+		gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+		image_tag(gravatar_url, alt: user.username, class: 'img-circle')
+	end
 end
