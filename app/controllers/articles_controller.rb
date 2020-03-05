dbb9427da3a74801791205b413bc9f6170d4a588
Riddhi Thummar <thummarriddhi8@gmail.com>
2020-03-05 09:26:23 -0500

    Update current user to be article owner

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,8 +15,7 @@ class ArticlesController < ApplicationController
 
 	def create
 		@article = Article.new(article_params)
-		# Hardcoded this temporarily
-		@article.user = User.find 12
+		@article.user = current_user
 		if @article.save
 			flash[:success] = "Article was successfully created!"
 			redirect_to article_path(@article)
